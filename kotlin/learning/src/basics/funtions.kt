3351819cc5996cc1c71c0a1f0a274679482640c6
Adriano de Azevedo <adrianopato.az@gmail.com>
2019-03-23 13:11:48 -0300

    Learning: basic functions

--- kotlin/learning/src/basics/funtions.kt
+++ kotlin/learning/src/basics/funtions.kt
@@ -1,2 +1,21 @@
 package basics
 
+fun sayHello() {
+    println("Hello")
+}
+
+fun toUpper(str: String): String {
+    return str.toUpperCase()
+}
+
+fun getLetter(str: String, index: Int = 0): Char {
+    return str[index]
+}
+
+fun main(args: Array<String>) {
+    sayHello()
+
+    println(toUpper("ASdfasdFASDfasdFASDfaSDF"))
+    println(getLetter("Adriano"))
+    println(getLetter("Adriano", 5))
+}
